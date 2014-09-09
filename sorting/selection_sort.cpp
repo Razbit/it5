302519133ffa740e41ba741c8c35384d67acc2ed
Razbit <razclocker@gmail.com>
2014-09-09 18:03:12 +0300

    Templatization works

--- sorting/selection_sort.cpp
+++ sorting/selection_sort.cpp
@@ -7,36 +7,53 @@
 #include <string>
 #include <iostream>
 
-std::string names[] = { "Parker", "Camden", "Carlos", "Preston",
-				   "James", "Stephen", "Harrison", "Robert",
-				   "Jack", "Lane", "Aden", "Mateo",
-				   "Gabriel", "Jakob", "David", "Everett",
-				   "Cade", "Landon", "Antonio", "Jesse",
-				   "Jared", "Tanner", "Emiliano", "Andy",
-				   "Zackary", "Steven", "Jordan", "Louis",
-				   "Steven", "Isaiah", "Giovanni", "Micah",
-				   "Braden", "Jameson", "Esteban", "Shawn",
-				   "Vincent", "Braxton", "Carlos", "Grant",
-				   "Bryson", "Cohen", "Jose", "Chance",
-				   "Dalton", "Allen", "Andre", "Christopher",
-				   "Roman", "Caleb"
+std::string names[] = {
+	"Parker", "Camden", "Carlos", "Preston",
+	"James", "Stephen", "Harrison", "Robert",
+	"Jack", "Lane", "Aden", "Mateo",
+	"Gabriel", "Jakob", "David", "Everett",
+	"Cade", "Landon", "Antonio", "Jesse",
+	"Jared", "Tanner", "Emiliano", "Andy",
+	"Zackary", "Steven", "Jordan", "Louis",
+	"Steven", "Isaiah", "Giovanni", "Micah",
+	"Braden", "Jameson", "Esteban", "Shawn",
+	"Vincent", "Braxton", "Carlos", "Grant",
+	"Bryson", "Cohen", "Jose", "Chance",
+	"Dalton", "Allen", "Andre", "Christopher",
+	"Roman", "Caleb"
 };
 
 template<typename key_t, typename val_t>
 struct node_t
 {
-    	key_t key;
+	key_t key;
 	val_t val;
 };
 
-void init_array(node_t<int, std::string array[]);
-void print(node_t<int, std::string array[]);
+void init_array(node_t<int, std::string> array[]);
+void print(node_t<int, std::string> array[]);
 
 template<typename key_t, typename val_t>
-void sel_sort_str(node_t array[])
+void sel_sort_str(node_t<key_t, val_t> array[]);
+
+int main()
+{
+	node_t<int, std::string> array[20];
+
+	init_array(array);
+	print(array);
+	std::cout << std::endl;
+	sel_sort_str(array);
+	print(array);
+
+	return 0;
+}
+
+template<typename key_t, typename val_t>
+void sel_sort_str(node_t<key_t, val_t> array[])
 {
 	int smallest_key_index;
-	node_t temp;
+	node_t<key_t, val_t> temp;
 	
 	for (int i = 0; i < 20; i++)
 	{
@@ -57,19 +74,6 @@ void sel_sort_str(node_t array[])
 			array[smallest_key_index] = temp;
 		}				
 	}
-}
-
-int main()
-{
-	node_t<int, std::string> array[20];
-
-	init_array(array);
-	print(array);
-	std::cout << std::endl;
-	sel_sort_str(array);
-	print(array);
-
-	return 0;
 }
 
 void init_array(node_t<int, std::string> array[])
